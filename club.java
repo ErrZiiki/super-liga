e2c0032ad78457fcc4d20c7f9d99983f185e7351
Diego de la Fuente <diegodelafuentealonso@gmail.com>
2023-02-28 12:14:59 +0100

    commit 1

---
+++ club.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class club here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class club
+{
+    // instance variables - replace the example below with your own
+    private String denomClub;
+    private boolean sancionado;
+
+    /**
+     * Constructor for objects of class club
+     */
+    public club()
+    {
+        
+    }
+}
